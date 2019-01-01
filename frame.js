453af66915b868ab41cca65ae7d8d58b94b9f8ed
Isaac Hew <isaachewks@gmail.com>
2019-01-01 06:38:49 +0000

    Added health bar g element

--- frame.js
+++ frame.js
@@ -240,6 +240,15 @@ function mode1(){
 					entdh=entdbox.height
 					entdis.attr("x",en.pos[0]).attr("y",en.pos[1])
 					.attr("transform","rotate("+en.rot+","+(en.pos[0]+entdw/2)+","+(en.pos[1]+entdh/2)+")")
+					if(en.hp<en.maxhp){
+						if($("#"+en.id+"hb").length){
+							
+						}else{
+							$("#objects").append(svgel("g",{"id":en.id+"hb"}))
+						}
+					}else if($("#"+en.id+"hb").length){
+						
+					}
 					if(entdis.attr("href")!=="#"+en.type){
 						entdis.remove()
 					}
