044132e5a5a4f4ee8a3ac88ff4c4fb7342266536
Isaac Hew <isaachewks@gmail.com>
2019-01-22 23:12:30 +0000

    Deleted doubled crespr from merge conflict

--- game.js
+++ game.js
@@ -120,34 +120,6 @@ function clev(el){
     en=(el.id[0]==="E")?entities[k]:buildings[k]
     if(en.type.slice(0,4)=="coll")da.collect=en.id
 }
-function crespr(ty){
-    sprhold=$("g#sprites")
-    if(!$("#"+ty,sprhold)[0]){
-        spri=$(svgel("g",{"id":ty,"class":"sprite"}))
-    	for(sh of rends[ty].split(" ")){
-    		st=sh[0]
-    		params=sh.slice(1).split(",")
-    		if(st=="R"){
-    			spri.append(svgel("rect",{
-    				"x":params[0],
-    				"y":params[1],
-    				"width":params[2],
-    				"height":params[3],
-    				"transform":(params[4]?"rotate("+params.slice(4,7).join(",")+")":"")}
-    				))
-    		}else if(st=="E"){
-    			spri.append(svgel("ellipse",{"cx":params[0],"cy":params[1],"rx":params[2]/2,"ry":params[3]/2}))
-    		}else if(st=="P"){
-    			spri.append(svgel("polygon",{"points":params.join(" ")}))
-    		}
-    		if(ty.slice(0,4)==="coll"){
-    			cols=["#777777","#ff0000"]
-    			$(spri).children().last().attr("fill",cols[ty.slice(4)]).attr("stroke","none")
-    		}
-    	}
-    	sprhold.append(spri)
-    }
-}
 function gbb(el){
     elu=$(el)[0].getBoundingClientRect()
     return {"x":elu.left+scrollX,"y":elu.top+scrollY,"width":elu.width,"height":elu.height}
